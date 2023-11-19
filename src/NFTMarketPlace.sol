b71b95dad7b46b3a1d8ad0b32cd85ae9024fe3d0
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-11-18 21:16:18 -0600

    adding sell function

--- src/NFTMarketPlace.sol
+++ src/NFTMarketPlace.sol
@@ -23,6 +23,17 @@ contract NFTMarketplace {
         address seller;
         bool isSold;
         uint256 price;
+        address nftContract;
+        uint256 tokenId;
+    }
+
+    mapping(uint256 => Order) public orders;
+
+    function sell(uint256 id, address nftContract, uint256 tokenId, uint256 price) external {
+        require(price > 0, "Price must be at least 1 wei");
+        require(orders[id].isSold == false, "This order has already been sold");
+        require(orders[id].seller == address(0), "This order has already been created");
+        orders[id] = Order(id, msg.sender, false, price, nftContract, tokenId);
     }
 
 
